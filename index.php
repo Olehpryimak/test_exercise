3f69315f47f17d39732a497087dca803f2b8f328
OlehPryimak <o380979848493o@ukr.net>
2019-05-27 22:17:07 +0300

    Create form

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+
+<html lang="ru">
+    <head>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Тестове завдання</title>
+
+        <link href="/public/css/bootstrap.min.css" rel="stylesheet">
+        <script src="/public/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="row col-md-3">
+
+            </div>
+            <div class="well col-md-6">
+                <h2 style="text-align: center">Укажите параметры</h2>
+                <div class="form-group">
+                    <form style="text-align: center" id="create_image" action="/" method="get">
+                        <br>
+                        <input type="number" name="width" value="" min="0" class="form-control" placeholder="Image width" required >
+                        <br>
+                        <input type="number" name="height" value="" min="0" class="form-control" placeholder="Image height" required >
+                        <br>
+                        <input type="number" name="radius" value="" min="0" class="form-control" placeholder="Circle radius" required >
+                        <br>
+                        <div style="text-align: left"> Количество окружностей:</div>
+                        <br>
+                        <select size="1" title="circle_count"  name="count" class="form-control" required >
+                            <option >5</option>
+                            <option >10</option>
+                            <option >20</option>
+                            <option >50</option>
+                        </select>
+                        <br>
+                        <button type="submit"  class="btn btn-success" name="create_image">Сгенерировать изображение</button>
+                    </form>
+                </div>
+            </div>
+    </body>
+</html>
